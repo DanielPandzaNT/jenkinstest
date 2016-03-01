d076187ef58d8adce074d37788cd3c2efeb00cb9
Daniel Pandza <daniel.pandza@novatec-gmbh.de>
2016-03-01 15:06:27 +0100

    new

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+///git@github.com:DanielPandzaNT/jenkinstest.git
+
+public class HelloWorld 
+{
+ 
+       public static void main (String[] args)
+       {
+             // Ausgabe Hello World!
+             System.out.println("Hello World!");
+       }
+}
